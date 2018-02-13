1ec1237375291d246f455aff4c6251b42b9a82fe
Naser Erwemi <aerwemi@me.com>
2018-02-13 14:00:34 -0600

    init4

--- static/js/app.js
+++ static/js/app.js
@@ -5,17 +5,18 @@ var url = "/names";
 function init() {
     Plotly.d3.json(url, function(error, names) 
     {
-        console.log(names);
+        console.log(names[0]);
 
-    Plotly.d3.select('#selDataset')
-    .selectAll('option')
-    .data(names)
-    .enter()
-    .append('option')
-    .on("change", optionChanged)
-    .attr("value", function(d){return d;})
-    
-    console.log(d);
+        Plotly.d3.select('#selDataset')
+        .selectAll('option')
+        .data(names)
+        .enter()
+        .append('option')
+        .on("change", function() {
+            var name = d3.select(this).node().value;
+            console.log(name);
+        })
+        .attr("value", function(d){return d;})
 
 
     
