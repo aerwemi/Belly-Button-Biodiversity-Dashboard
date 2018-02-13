8910a750542093c3ba0b9738e9ed52897e634301
Naser Erwemi <aerwemi@me.com>
2018-02-13 13:22:29 -0600

    init2

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,36 @@
+  
 /* data route */
 var url = "/names";
 
 function init() {
     Plotly.d3.json(url, function(error, names) 
     {
-        console.log(names);
-
-        
+        console.log(names[0]);
 
     Plotly.d3.select('#selDataset')
     .selectAll('option')
     .data(names)
     .enter()
     .append('option')
+    .attr("value", function(d){return d;})
+
+
+    
     });
 
 }
 
-console.log(names)
+
+function optionChanged() {
+    var name = document.getElementById("selDataset").value;
+
+    // Initialize empty arrays to contain our axes
+    // var name = this.value;
+    console.log(name);
+  
+    //_Plotly(name);
+  };
+
+
 
 init();
-
-
-    
